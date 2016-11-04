5611f18dee168a275a12132bc5aa01568d0d8e35
nisi credideritis non intelligentis <hanbiao4079@sina.com>
2016-11-04 17:04:38 +0800

    myWebStore

---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>首页</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="style/commond.css">
+<link rel="stylesheet" href="style/index.css">
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div id="main">
+		
+		<div class="header_top">
+			<span><a href="">退出</a></span>
+			<span><a href="">登录</a></span>
+			
+		</div>
+		<div class="logo">
+			<img src="image/logo.jpg" alt="">
+			<div class="logo_right">
+				<input type="text" placeholder="搜索">
+				<div class="logo_right_a">
+					<a class="active" href="">我的订单</a>
+					<a href="">购物车</a>
+				</div>
+			</div>
+		</div>
+		
+		<nav id="navbar">
+			<div class="left_nav">
+				<p>全部书籍分类</p><img src="" alt="">
+				<ul>
+					<li><a href=""></a></li>
+				</ul>
+			</div>
+			<div class="right_nav">
+				<ul>
+					<li><a href="">内科</a></li>
+					<li><a href="">外科</a></li>
+					<li><a href="">儿科</a></li>
+					<li><a href="">考研</a></li>
+					<li><a href="">病理</a></li>
+					<li><a href="">医学杂志订阅</a></li>
+				</ul>
+			</div>
+		</nav>
+		
+		<aside class="aside">
+			<nav class="aside_nav">
+				<ul>
+					<li><a href="">内科</a>、<a href="">内科</a></li>
+					<li><a href="">外科</a>、<a href="">内科</a>、<a href="">内科</a></li>
+					<li><a href="">儿科</a>、<a href="">内科</a></li>
+					<li><a href="">考研</a>、<a href="">内科</a></li>
+					<li><a href="">病理</a>、<a href="">内科</a></li>
+					<li><a href="">医学杂志订阅</a></li>
+					<li><a href="">内科</a>、<a href="">医学杂志订阅</a></li>
+					<li><a href="">外科</a>、<a href="">医学杂志订阅</a></li>
+					<li><a href="">儿科</a>、<a href="">医学杂志订阅</a></li>
+					<li><a href="">考研</a>、<a href="">医学杂志订阅</a></li>
+					<li><a href="">病理</a>、<a href="">医学杂志订阅</a></li>
+					<li><a href="">医学杂志订阅</a></li>
+				</ul>
+			</nav>
+		</aside>
+		
+		<section class="section">
+			<img src="image/3.jpg" alt="" width="750" height="400">
+		</section>
+		
+		<div class="left_new">
+			
+		</div>
+		
+		<div class="right_new">
+			
+		</div>
+		
+		<div class="link">
+			
+		</div>
+		
+		<footer class="footer">
+			
+		</footer>
+		
+	</div>
+</body>
+</html>
